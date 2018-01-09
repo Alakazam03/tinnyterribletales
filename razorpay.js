81972bc12004cf6f026e9c8e39935cc61163ab01
vaibhav aggarwal <vaibhav.045@iiitb.org>
2018-01-09 23:47:46 +0530

    Add files via upload

--- razorpay.js
+++ razorpay.js
@@ -2,19 +2,18 @@ function done(){
   function getlength(number){
     return number.toString().length;
   }
-  phoneUser=Number(document.getElementsByName('phone')[0].value);
-  //addUser=addUser.replace(\\g, '-');
-  if(phoneUser=="")
-    alert("Invald phone number!");
+  User=Number(document.getElementsByName('number')[0].value);
+  if(User=="")
+    alert("Invald Number");
   else{
     $.ajax({
       type: "GET",
-      url: 'https://7b493696.ngrok.io/',
+      url: 'https://df09069c.ngrok.io',
       data: {
-        "id": phoneUser,
+        "id": User,
       },
       success: function(data){
-        console.log(data);
+        //console.log(data);
         data=JSON.parse(data);
         data=JSON.parse(data);
         var x=document.getElementById('tableGen');
@@ -31,12 +30,10 @@ function done(){
         console.log(data);
       }
     });
-  //  alert("Close the webview to proceed! You'll receive a confirmation message soon.");
 
   }
 }
 
-var price, nameUser, phoneUser, addUser;
 
 function getURL(){
   var str = window.location.href;
@@ -56,70 +53,3 @@ function getURL(){
   console.log(str);
   console.log(uid);
 }
-
-/*
-function razorpay(){
-  var options = {
-    "key": "rzp_test_HMAxicoOIZn8Xx",
-    "amount": price*100, // 2000 paise = INR 20
-    "name": "Baba da Dhaba",
-    "description": "Payment for your food order",
-    "image": "",
-    "handler": function (response){
-      console.log(response);
-      //save payment id
-      $.ajax({
-        type: "GET",
-        url: redisDb+"/"+uid+"/set_payment_key/"+response.razorpay_payment_id,
-        success: function(data){
-          console.log('Success!');
-        },
-        error: function(data){
-          console.log('Nope!');
-        }
-      });
-      //alert(response.razorpay_payment_id);
-      //save address
-      $.ajax({
-        type: "GET",
-        url: redisDb+"/"+uid+"/set_address/payment/"+lat+","+long,
-        success: function(data){
-          console.log('Success!');
-        },
-        error: function(data){
-          console.log('Nope!');
-        }
-      });
-      //to NodeJS script
-      $.ajax({
-        type: "GET",
-        url: "https://8dd5fd02.ngrok.io/payments",
-        data: {
-          'key': response.razorpay_payment_id,
-          'Id': uid
-        },
-        success: function(data){
-          console.log(data);
-        },
-        error: function(data){
-          console.log('Nope!');
-        }
-      });
-      alert("Close webview to proceed!");
-    },
-    "prefill": {
-      "name": nameUser,
-      "email": emailUser,
-      "contact": phoneUser
-    },
-    "notes": {
-      "address": document.getElementsByName('address')[0].value
-    },
-    "theme": {
-      "color": "#77cde3"
-    }
-  };
-  var rzp1 = new Razorpay(options);
-  rzp1.open();
-}
-*/
